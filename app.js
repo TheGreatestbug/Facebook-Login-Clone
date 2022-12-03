fdf288869cb17e8688b35f0b3c467d99e43d98fd
Great Ekwueme <izyspikes@gmail.com>
2022-12-03 03:18:37 +0100

    Changed app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const todo = [
+   {
+      id: 1,
+      job: ' Meeting with Boss',
+      isCompleted: false,
+   },
+   {
+      id: 2,
+      job: 'Bath Dogs',
+      isCompleted: false,
+   },
+   {
+      id: 3,
+      job: ' Cut Nails',
+      isCompleted: true,
+   }
+]
